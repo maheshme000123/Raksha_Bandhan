172c998f449a7c22c554fa856250370ab13e1472
maheshme000123 <161828282+maheshme000123@users.noreply.github.com>
2024-08-05 19:59:27 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("My Queen !", 28, 10);
+            ctx.fillText("POTTI !", 28, 10);
             ctx.restore();
         },
         clear: function() {
